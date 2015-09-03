c640fe46e0b8011b41176b423a2957f2defafd18
Vicente de Alencar <vicentedealencar@gmail.com>
2015-09-03 01:38:40 -0300

    persist one document per reducer

--- src/index.js
+++ src/index.js
@@ -1,77 +1,131 @@
 import PouchDB from 'pouchdb';
+import equal from 'deep-equal';
+import 'array.from';
 
-const DB_CHANGES = 'DB_CHANGES';
+const SET_REDUCER = 'SET_REDUCER';
 const db = new PouchDB('app');
-const docId = 'redux-store';
-const emptyDoc = {_id: docId};
+let isInitialized = false;
 
 export const persistentStore = storeCreator => (reducer, initialState) => {
   const store = storeCreator(reducer, initialState);
 
-  db.changes({include_docs: true}).on('change', function(change) {
-    store.dispatch({ type: DB_CHANGES, ...change.doc});
+  db.changes({include_docs: true}).on('change', change => {
+    isInitialized = true;
+    const { _id, _rev, state } = change.doc;
+
+    const action = {
+      type: SET_REDUCER,
+      reducer: _id,
+      state: state
+    };
+
+    store.dispatch(action);
   });
 
   return store;
 };
 
-export const persistentReducer = reducer => (state, action) => {
-  if (action.type === DB_CHANGES && action[reducer.name]) {
-    return action[reducer.name]; //short-circuit reducer
-  }
+export const persistentReducer = reducer => {
+  let lastState;
 
-  const nextState = reducer(state, action);
+  return (state, action) => {
+    if (action.type === SET_REDUCER &&
+        action.reducer === reducer.name) {
+      return reducer(action.state, action);
+    }
 
-  persistState({[reducer.name]: nextState});
+    const reducedState = reducer(state, action);
 
-  return nextState;
+    if (isInitialized && !equal(reducedState,lastState)) {
+      lastState = reducedState;
+
+      save(reducer.name)(reducedState);
+    }
+
+    return reducedState;
+  };
 };
 
-let unpersistedState = null;
-let isUpdating = false;
-const persistState = (nextState) => {
-  if (isUpdating) {
-    unpersistedState = {
-      ...unpersistedState,
-      ...nextState
-    };
-
-    return;
-  }
-
-  isUpdating = true;
-
-  getState().then(doc => {
-    const newDoc = {
-      ...doc,
-      ...unpersistedState,
-      ...nextState
-    };
-
-    // console.log('old', doc);
-    // console.log('new', newDoc);
-    db.put(newDoc).catch(error => {
-      console.error(error);
-    }).then(() => {
-      isUpdating = false;
-
-      if (unpersistedState) {
-        persistState(unpersistedState);
-      }
-    });
-
-    unpersistedState = null;
-  });
-};
-
-const getState = () => {
-  return db.get(docId).catch(err => {
+const load = _id => {
+  return db.get(_id).then(x => {
+    // console.log('loaded: ', _id);
+    // console.table(x);
+    return x;
+  }).catch(err => {
     if (err.status === 404) {
-      return db.put(emptyDoc).then(() => db.get(docId));
+      return db.put({_id: _id}).then(() => db.get(_id));
     } else {
       throw err;
     }
   });
-}
+};
 
-window.destroyDb = () => db.destroy();
+let unpersistedState = {};
+let isUpdating = {};
+const save = reducerName => {
+  const saveReducer = reducerState => {
+    if (isUpdating[reducerName]) {
+      if (Array.isArray(reducerState)) {
+        unpersistedState[reducerName] = [
+          ...(unpersistedState[reducerName] || []),
+          ...reducerState
+        ];
+      } else {
+        unpersistedState[reducerName] = {
+          ...unpersistedState[reducerName],
+          ...reducerState
+        };
+      }
+
+      return;
+    }
+
+    isUpdating[reducerName] = true;
+
+    load(reducerName).then(doc => {
+
+      let newState;
+      if (Array.isArray(reducerState)) {
+        newState = [
+          ...(doc.state || []),
+          ...(unpersistedState[reducerName] || []),
+          ...reducerState
+        ];
+      } else {
+        newState = {
+          ...doc.state,
+          ...unpersistedState[reducerName],
+          ...reducerState
+        };
+      }
+
+      const newDoc = {
+        ...doc,
+        state: newState
+      };
+
+      unpersistedState[reducerName] = null;
+
+      db.put(newDoc).catch(error => {
+        console.error(error);
+      }).then(() => {
+        // console.log('puted: ', newDoc._id);
+        // console.table(newDoc);
+        isUpdating[reducerName] = false;
+
+        if (unpersistedState[reducerName]) {
+          saveReducer(unpersistedState[reducerName]);
+        }
+      });
+    });
+  };
+
+  return saveReducer;
+};
+
+//super powers
+window.db = db;
+
+window.allDocs = () =>
+  db.allDocs({include_docs: true})
+  .then(x => console.log(x));
